8afb5d505bfe2dc40c6dc896f03c389bc2e89c2c
Gabriel T <gab.toledo@duocuc.cl>
2025-06-10 01:38:02 -0400

    inicializamos el proyecto - incializa proyecto - agrega README.md

---
+++ gestion-producto/src/test/java/com/perfulandia/producto/GestionDeProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
