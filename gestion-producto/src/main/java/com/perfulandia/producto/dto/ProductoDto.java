f95d7706c62e38edec8151e6913d7cbf81e3a1ab
camilo_workspace_model/controller/service <camilovsprogram355@gmail.com>
2025-07-02 20:38:31 -0400

    Integracion de hateoas/Swagger

--- gestion-producto/src/main/java/com/perfulandia/producto/dto/ProductoDto.java
+++ gestion-producto/src/main/java/com/perfulandia/producto/dto/ProductoDto.java
@@ -1,15 +1,19 @@
 package com.perfulandia.producto.dto;
 
+import org.springframework.hateoas.RepresentationModel;
 import lombok.*;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductoDto {
+public class ProductoDto extends RepresentationModel<ProductoDto> {
+
     private Integer id;
     private String nombre;
     private String descripcion;
     private Double precioUnitario;
     private String categoria;
     private Boolean activo;
+    
 }
